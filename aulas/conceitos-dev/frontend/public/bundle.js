935212475d57cf789d2706fefc386e88aac9a0fb
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2024-09-21 18:21:11 -0400

    First commit

---
+++ aulas/conceitos-dev/frontend/public/bundle.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var soma = function soma(a, b) {
+  return a + b;
+};
+console.log(soma(1, 2));
---
+++ aulas/conceitos-dev/frontend/public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter (transpliar) código React para um código que o browser entenda
+ * WebPack: Para cada tipo de arquivo (.js, .css, .png) converte o código de uma mandeira diferente
+ * Loaders: babel-loader, css-loader, image-loader
+ *
+ */
