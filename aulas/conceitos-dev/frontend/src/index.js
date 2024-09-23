da66c90f70608f14498ff4acff37e9bee761fc0d
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2024-09-23 14:09:32 -0400

    Corrigido arquivo src/index.js

--- aulas/conceitos-dev/frontend/src/index.js
+++ aulas/conceitos-dev/frontend/src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-<<<<<<< HEAD
-// JSX: HTML dentro do JavaScript (JavaScript XML)
 render(<App />, document.getElementById('app'));
-=======
-console.log(soma(1, -2));
->>>>>>> ff034af60825e704678df38959c82a8be0748da8
