d5da445d647d58f9b4f6368bf2be215501ce9724
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2024-09-23 14:02:48 -0400

    Adicionado primeiro componente

--- aulas/conceitos-dev/frontend/src/index.js
+++ aulas/conceitos-dev/frontend/src/index.js
@@ -1,5 +1,10 @@
-const soma = (a, b) => {
-  return a + b;
-};
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
+<<<<<<< HEAD
+// JSX: HTML dentro do JavaScript (JavaScript XML)
+render(<App />, document.getElementById('app'));
+=======
 console.log(soma(1, -2));
+>>>>>>> ff034af60825e704678df38959c82a8be0748da8
