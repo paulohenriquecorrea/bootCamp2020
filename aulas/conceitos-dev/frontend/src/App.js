a95aea6ffa124c76a94e7dd12d5772746a90f106
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2024-09-24 12:03:50 -0400

    Aplicado primeiro conceito de imutabilidade (useState)

--- aulas/conceitos-dev/frontend/src/App.js
+++ aulas/conceitos-dev/frontend/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState([
+    'Desenvolvimento de app',
+    'Front-end web',
+  ]);
+
+  function handleAddProjects() {
+    // projects.push(`Novo Projeto ${Date.now()}`);
+
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    console.log(projects);
+  }
   return (
     <>
       <Header title="Homepage">
@@ -11,11 +22,16 @@ function App() {
           <li>Login</li>
         </ul>
       </Header>
-      <Header title="Project">
-        <ul>
-          <li>Apenas 1</li>
-        </ul>
-      </Header>
+
+      <Header title="Project" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProjects}>
+        Adicionar Projeto
+      </button>
     </>
   );
 }
