bd64aedc23821575ea980157cbc7ea2de69c04b0
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2024-09-23 23:11:18 -0400

    Adicionada primeira propriedade

--- aulas/conceitos-dev/frontend/src/App.js
+++ aulas/conceitos-dev/frontend/src/App.js
@@ -4,8 +4,18 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Project</li>
+          <li>Info</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+      <Header title="Project">
+        <ul>
+          <li>Apenas 1</li>
+        </ul>
+      </Header>
     </>
   );
 }
