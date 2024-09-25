c68f5a8abc640574a4fcb0dacd8bc0c80bb2022e
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2024-09-25 11:41:48 -0400

    Iniciada a crianção do projeto backend

---
+++ aulas/conceitos-dev/backend/src/index.js
@@ -0,0 +1,11 @@
+const { json } = require('express');
+const express = require('express');
+// import { express } from 'express';
+
+const app = express();
+
+app.get('/projects', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+app.listen(3333);
