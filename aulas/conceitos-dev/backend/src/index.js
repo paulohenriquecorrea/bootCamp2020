0075d94b689a63499bd0b2f9708149433d4ed9fa
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2024-09-25 11:53:42 -0400

    Adicionada a dependência de desenvolvimento nodemon

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -8,4 +8,6 @@ app.get('/projects', (request, response) => {
   return response.json({ message: 'Hello World' });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('🚀 Back-end started! 🚀');
+});
