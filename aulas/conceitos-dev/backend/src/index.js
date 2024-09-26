57431b0059d9307c26717333fa9e3c63c1290a4a
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2024-09-25 21:41:34 -0400

    Adicionado recebimento dos três principais tipos de parâmetros (Query Params, Route Params, Request Body)

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -4,19 +4,28 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  // const query = request.query;
+  const { name, owner } = request.query;
+
   return response.json(['Projeto 1', 'Projeto 2']);
 });
 
 app.post('/projects', (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3']);
 });
 
 app.put('/projects/:id', (request, response) => {
+  const params = request.params;
+  console.log(params);
   return response.json(['Projeto 4', 'Projeto 2', 'Projeto 3']);
 });
 
-app.delete('/projects/:id', () => {
+app.delete('/projects/:id', (request, response) => {
   return response.json(['Projeto 2']);
 });
 app.listen(3333, () => {
